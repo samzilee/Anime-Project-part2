a8feb0c374e64b3a4860f6e3e78e45e807bfe2b6
Your Name <you@example.com>
2024-08-26 22:48:45 +0100

    modified:   config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const sideMenu = document.getElementById("menu");
 const sideBar = document.getElementById("sideBar");
 const cancelSideBar = document.getElementById("cancelSideBar");
@@ -7,7 +8,7 @@ let sideBarWidth = "";
 const showSideBar = () => {
   sideBar.style.width = "70%";
   sideBarWidth = sideBar.style.width;
-
+  body.style.overflowY = "hidden";
   setTimeout(() => {
     if (sideBarWidth === "70%") {
       allContent.addEventListener("click", cancleBar, { once: true });
@@ -17,6 +18,7 @@ const showSideBar = () => {
 
 const cancleBar = () => {
   sideBar.style.width = "0";
+  body.style.overflowY = "scroll";
   return (sideBarWidth = "0");
 };
 
